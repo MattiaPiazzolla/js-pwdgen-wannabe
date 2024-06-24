bb0dcd7a92d50b09ed56d41153eb14c144175c7a
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-24 17:54:56 +0200

    [COMPLETED] completed bonus with styling and some hover effect

--- BONUS/script.js
+++ BONUS/script.js
@@ -7,6 +7,10 @@ const color = prompt('Inserisci il tuo colore preferito');
 const password = `${name}${surname}${color}23`;
 
 // output phrase
-document.getElementById('passwordPhrase').innerHTML = `La tua password è ${password}`;
+document.getElementById('passwordPhrase').innerHTML = `La tua password è <span class="text-danger fw-bold not-safe" data-bs-toggle="tooltip" title="Questa password non è sicura!">${password}</span>`;
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 }
+
