b5c78d5271177dc4b9b401b6923530622ee43421
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-24 16:08:29 +0200

    [FEAT] added the prompt to get data in script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get data using prompt
+const name = prompt('Inserisci il tuo nome');
+const surname = prompt('Inserisci il tuo cognome');
+const color = prompt('Inserisci il tuo colore preferito');
+const password = `${name}${surname}${color}23`;
