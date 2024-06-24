81cfd8d35c17f817a8a1adf1eebfe634ebe1f214
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-24 16:11:32 +0200

    [BONUS] added a button and experimented with the function in javascript to make the propmt appear on click of the button

---
+++ BONUS/script.js
@@ -0,0 +1,12 @@
+function generatePassword (){
+
+// Get data using prompt
+const name = prompt('Inserisci il tuo nome');
+const surname = prompt('Inserisci il tuo cognome');
+const color = prompt('Inserisci il tuo colore preferito');
+const password = `${name}${surname}${color}23`;
+
+// output phrase
+document.getElementById('passwordPhrase').innerHTML = `La tua password è ${password}`;
+
+}
