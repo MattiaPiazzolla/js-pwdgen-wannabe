d2014dc076052f60fb8b950b4a4733bfd55cce6a
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-24 16:09:32 +0200

    [FEAT] added a phrase with the results of the concatenation of the information

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const name = prompt('Inserisci il tuo nome');
 const surname = prompt('Inserisci il tuo cognome');
 const color = prompt('Inserisci il tuo colore preferito');
 const password = `${name}${surname}${color}23`;
+
+// output phrase
+document.getElementById('passwordPhrase').innerHTML = `La tua password è ${password}`;
